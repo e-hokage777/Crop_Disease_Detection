a536675c9645410c75ae5e368248695d15e03526
e.hokage777 <e.hokage777@gmail.com>
2024-11-11 23:07:37 +0000

    working on backbone path

--- scripts/config.py
+++ scripts/config.py
@@ -1,3 +1,5 @@
+import os
+
 ## training
 MIN_EPOCHS = 1
 MAX_EPOCHS = 10
@@ -14,4 +16,6 @@ BATCH_SIZE = 4
 ## others
 LOGS_PATH = "../tb_logs"
 CHECKPOINT_SAVEPATH = "../checkpoints/"
-BACKBONE_PATH = "../trained_backbones/mobilenet_v3_large=0.80.pt"
+BACKBONE_PATH = os.path.join(
+    os.path.abspath(__file__), "../trained_backbones/mobilenet_v3_large=0.80.pt"
+)
