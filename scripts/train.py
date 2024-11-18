8348946d8ca6579c7f43efa627238602aa0d22d2
ehokage777 <e.hokage777@gmail.com>
2024-11-18 10:56:19 +0000

    fixed bug

--- scripts/train.py
+++ scripts/train.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     parser.add_argument("--learning_rate", type=float, default=0.008)
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--min_epochs", type=int, default=1)
-    parser.add_argument("--max_epochs", type=int, defualt=20)
+    parser.add_argument("--max_epochs", type=int, default=20)
     parser.add_argument("--num_workers", type=int, default=0)
     parser.add_argument("--precision", type=str, default="16-mixed")
     parser.add_argument("--persistent_workers", type=bool, default=False)
